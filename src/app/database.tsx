4c4a39e3658f0b35a97c345b6fed2e9ab92bce08
Stepan Pyriaev <s.pyriaev@yahoo.com>
2023-12-01 19:09:35 +0300

    add generators

--- src/app/database.tsx
+++ src/app/database.tsx
@@ -30,7 +30,10 @@ export class IndexedDBHandler {
   
     private createObjectStore() {
       if (this.db) {
-        this.db.createObjectStore(this.storeName, { keyPath: 'id', autoIncrement: true });
+        const objectStore = this.db.createObjectStore(this.storeName, { keyPath: 'mid', autoIncrement: false });
+        objectStore.createIndex('conversationId', 'conversationId', { unique: false });
+        objectStore.createIndex('date', 'date', { unique: false });
+        objectStore.createIndex('[source + conversationId + date]', ['source', 'conversationId', 'date'], { unique: false });
       }
     }
   
@@ -45,7 +48,7 @@ export class IndexedDBHandler {
         const objectStore = transaction.objectStore(this.storeName);
   
         for (let i = 0; i < rowCount; i++) {
-          const newRow = { /* your data object here */ };
+          const newRow = generateRandomMessage();
           const request = objectStore.add(newRow);
   
           request.onerror = (event) => {
@@ -58,7 +61,152 @@ export class IndexedDBHandler {
         };
       });
     }
+
+    getMessagesWithinTimeRange(startTime: number, endTime: number): Promise<Message[]> {
+      return new Promise((resolve, reject) => {
+        if (!this.db) {
+          reject(new Error('Database not open'));
+          return;
+        }
+        const requestStartTime = Date.now();  
+        const transaction = this.db.transaction([this.storeName], 'readonly');
+        const objectStore = transaction.objectStore(this.storeName);
+        const range = IDBKeyRange.bound(startTime, endTime);
+  
+        const request = objectStore.index('date').openCursor(range);
+        const messages: Message[] = [];
+  
+        request.onsuccess = (event) => {
+          const cursor = (event.target as IDBRequest).result;
+          if (cursor) {
+            messages.push(cursor.value);
+            cursor.continue();
+          } else {
+            const requestEndTime = Date.now();
+            const requestTime = requestEndTime - requestStartTime;
+            console.log(`Request completed in ${requestTime} ms`);
+            resolve(messages);
+          }
+        };
+  
+        request.onerror = (event) => {
+          reject((event.target as IDBRequest).error);
+        };
+      });
+    }
   }
   
+  type Message = {
+    clock: number,
+    conversationId: string,
+    date: number,
+    editedAt: number,
+    forward: number[],
+    hostPeerId: number,
+    isTempMessage: boolean,
+    messageContent: MessageContent,
+    mid: string,
+    prevMid: string,
+    randomId: string,
+    refType: string | undefined,
+    reply: string[],
+    senderPeerId: string | undefined,
+    senderUid: number,
+    source: string,
+    state: string,
+    threadId: string
+  }
 
+  type MessageContent = {
+    text: TextContent,
+    type: "text"
+  }
+
+  type TextContent = {
+    markdown: boolean,
+    media: string[],
+    mentions: string[],
+    text: string
+  }
+
+  function generateRandomMessage(): Message {
+    const randomMessage: Message = {
+      clock: generateRandomTimestampWithinLastMonth(),
+      conversationId: generateRandomString(),
+      date: Date.now(),
+      editedAt: Date.now() - Math.floor(Math.random() * 100000),
+      forward: Array.from({ length: Math.floor(Math.random() * 5) }, () => Math.floor(Math.random() * 100)),
+      hostPeerId: Math.floor(Math.random() * 100),
+      isTempMessage: Math.random() < 0.5,
+      messageContent: {
+        text: {
+          markdown: Math.random() < 0.5,
+          media: Array.from({ length: Math.floor(Math.random() * 3) }, () => generateRandomString()),
+          mentions: Array.from({ length: Math.floor(Math.random() * 3) }, () => generateRandomString()),
+          text: generateRandomTextMessage()
+        },
+        type: "text"
+      },
+      mid: generateMid(),
+      prevMid: generateRandomString(),
+      randomId: generateRandomString(),
+      refType: Math.random() < 0.5 ? generateRandomString() : undefined,
+      reply: Array.from({ length: Math.floor(Math.random() * 5) }, () => generateRandomString()),
+      senderPeerId: Math.random() < 0.5 ? generateRandomString() : undefined,
+      senderUid: Math.floor(Math.random() * 100),
+      source: generateRandomString(),
+      state: generateRandomString(),
+      threadId: generateRandomString()
+    };
   
+    return randomMessage;
+  }
+
+  export function generateRandomTimeRangeWithinLastMonth(): { startTime: number, endTime: number } {
+    const currentDate = new Date();
+    const currentTimestamp = currentDate.getTime();
+    const oneMonthInMillis = 30 * 24 * 60 * 60 * 1000; // Approximate milliseconds in a month
+  
+    const randomStartTimestamp = currentTimestamp - Math.floor(Math.random() * oneMonthInMillis);
+    const randomEndTimestamp = randomStartTimestamp + Math.floor(Math.random() * oneMonthInMillis);
+  
+    return { startTime: randomStartTimestamp, endTime: randomEndTimestamp };
+  }
+
+  function generateMid(): string {
+    const randomPart1 = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER);
+    const randomPart2 = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER);
+    return `-${randomPart1}|${randomPart2}`;
+  }
+
+  function generateRandomTextMessage(): string {
+    const messageLength = Math.floor(Math.random() * 1100) + 100; // Generates a length between 100 and 1200 characters
+    const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_-+=<>?/{}[]';
+  
+    let randomMessage = '';
+  
+    for (let i = 0; i < messageLength; i++) {
+      const randomIndex = Math.floor(Math.random() * characters.length);
+      randomMessage += characters.charAt(randomIndex);
+    }
+  
+    return randomMessage;
+  }
+
+  function generateRandomTimestampWithinLastMonth(): number {
+    const currentDate = new Date();
+    const currentTimestamp = currentDate.getTime();
+    const oneMonthInMillis = 30 * 24 * 60 * 60 * 1000; // Approximate milliseconds in a month
+  
+    const randomTimestamp = currentTimestamp - Math.floor(Math.random() * oneMonthInMillis);
+    return randomTimestamp;
+  }
+
+  function generateRandomString(length: number = 8): string {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+    for (let i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return result;
+  }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,13 @@ import Image from 'next/image'
 import styles from './page.module.css'
 import { Button } from '@mui/material'
 import { IndexedDBHandler } from './database'
+import { generateRandomTimeRangeWithinLastMonth } from './database'
 
 export default function Home() {
-  const handleClickButton = () => {
-      // Example usage:
+  const handleClickGenerateRecords = () => {
   const dbName = 'chat';
   const storeName = 'message';
-  const rowsToInsert = 1000;
+  const rowsToInsert = 100000;
   
   const dbHandler = new IndexedDBHandler(dbName, storeName);
   
@@ -24,10 +24,31 @@ export default function Home() {
     });
   }
 
+  const handleButtonRequestRescords = () => {
+    const dbName = 'chat';
+    const storeName = 'message';
+    const dbHandler = new IndexedDBHandler(dbName, storeName);
+
+    dbHandler.openDatabase()
+    .then(() => {
+      const requests = Array.from({ length: 100 }, () => {
+        const {startTime, endTime} = generateRandomTimeRangeWithinLastMonth()        
+        return dbHandler.getMessagesWithinTimeRange(startTime, endTime)
+      });
+  
+      const results = Promise.all(requests);
+      console.log('Messages within the time range:', results);
+    })
+    .catch((error) => {
+      console.error(`Error: ${error}`);
+    });
+  }
+
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <Button onClick={handleClickButton} variant="outlined">Generate data</Button>
+        <Button onClick={handleClickGenerateRecords} variant="outlined">Generate data</Button>
+        <Button onClick={handleButtonRequestRescords} variant="outlined">Generate requests</Button>
       </div>
     </main>
   )
